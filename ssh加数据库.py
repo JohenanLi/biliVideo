83f3e8aba3915ee96832ed4504d373b85cd55eaf
JohenanLi <599192367@qq.com>
2021-04-21 11:21:31 +0800

    Update ssh加数据库.py

--- ssh加数据库.py
+++ ssh加数据库.py
@@ -2,7 +2,7 @@ import paramiko
  
 ssh = paramiko.SSHClient()#创建SSH对象
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())#允许连接不在know_hosts文件中的主机
-ssh.connect(hostname='112.74.173.175', port=22, username='root', password='lywh978216-')#连接服务器
+ssh.connect(hostname='*', port=22, username='root', password='password')#连接服务器
  
 stdin, stdout, stderr = ssh.exec_command('whoami')#执行命令并获取命令结果
 #stdin为输入的命令
@@ -14,7 +14,7 @@ print(result)
 import MySQLdb
 
 # 打开数据库连接
-db = MySQLdb.connect(host='112.74.173.175',  
+db = MySQLdb.connect(host='*',  
             port=3306,  
             user='root',  
             passwd='12345678', 
